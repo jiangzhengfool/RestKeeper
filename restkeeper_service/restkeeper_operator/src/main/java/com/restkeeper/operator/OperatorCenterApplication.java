47b49cfd928dc8e033cc8e3725695505e7eb77fa
jiangzhengfu <jiangzhengfu@mchz.com.cn>
2023-10-20 11:09:45 +0800

    init

--- restkeeper_service/restkeeper_operator/src/main/java/com/restkeeper/operator/OperatorCenterApplication.java
+++ restkeeper_service/restkeeper_operator/src/main/java/com/restkeeper/operator/OperatorCenterApplication.java
@@ -1,9 +1,14 @@
 package com.restkeeper.operator;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.restkeeper"})
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.restkeeper.operator.mapper"})
 public class OperatorCenterApplication {
 
     public static void main(String[] args) {
